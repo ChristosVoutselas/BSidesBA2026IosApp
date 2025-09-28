503d1b812cb3f3c995630d87edd1a098e69849ad
christos <voutselas.christos@gmail.com>
2025-09-28 22:26:22 +0200

    push notification

---
+++ BSidesBA2026/AppDelegate.swift
@@ -0,0 +1,35 @@
+import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+        
+        Messaging.messaging().delegate = self
+        return true
+    }
+
+    // Get APNs token
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    // Get FCM token
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("✅ FCM token: \(fcmToken ?? "")")
+    }
+}
--- BSidesBA2026/BSidesBA2026App.swift
+++ BSidesBA2026/BSidesBA2026App.swift
@@ -6,10 +6,24 @@ import GoogleMobileAds
 @main
 struct BSidesBA2026App: App {
     
+  
+
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     init() {
-            // Start Google Mobile Ads SDK
+        // Start Google Mobile Ads SDK
         MobileAds.shared.start(completionHandler: nil)
+        
+        // Request push notification permissions
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
         }
+    }
         
 
     var body: some Scene {
